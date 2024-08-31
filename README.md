2477b2a48abc95011d01a3faf493bb0546eb30ab
bydlw98 <108581910+bydlw98@users.noreply.github.com>
2024-08-31 22:31:56 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# dwmblocks
+
+My build of [dwmblocks](https://github.com/torrinfail/dwmblocks) a modular status bar for dwm written in c.
+
+## Installation
+
+```
+git clone https://github.com/bydlw98/dwmblocks.git
+cd dwmblocks
+sudo make clean install
+```
---
+++ blocks.def.h
@@ -0,0 +1,9 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	{"", "date '+ %a, %d %b %Y  %I:%M %p '",					60,		0},
+};
+
+//sets delimiter between status commands. NULL character ('\0') means no delimiter.
+static char delim[] = " | ";
+static unsigned int delimLen = 5;
